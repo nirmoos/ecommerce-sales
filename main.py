84c5623af9b9a016af134f782e81589ee3a80ea6
Nirmal Hariharan <nirmalh@qburst.com>
2022-03-26 15:01:55 +0530

    Documentation added.

--- main.py
+++ main.py
@@ -33,7 +33,9 @@ CSV_TO_DB_KEY_TYPE_MAPPING = (
 
 
 class EcommerceSale:
+    """Ecommerce sale class allows to interact with sales data"""
     def __init__(self):
+        """Initializes the sql connection and cursor objects for database operations"""
         try:
             self.cnx = self.create_db_connection()
             self.cursor = self.cnx.cursor()
@@ -54,6 +56,7 @@ class EcommerceSale:
             sys.exit(1)
 
     def __del__(self):
+        """Closes the sql connection and cursor objects while closing the application"""
         try:
             self.cursor.close()
             self.cnx.close()
@@ -61,11 +64,16 @@ class EcommerceSale:
             pass
 
     def load_data(self, force_load):
+        """Handles the decision logic for the load operation"""
         self.cursor.execute(TABLE_CHECK_AND_ROW_COUNT_QUERY)
         result = list(self.cursor)
+
+        # Creates the table and load data if the table is not present
         if not result:
             self.cursor.execute(CREATE_TABLE_QUERY)
             self._load_csv_data_to_table()
+        # Truncate the table and load data, especially if the  force_load command-line option is provided as true
+        # Useful if we want to reload the table with the CSV after we have modified the table
         elif not result[0][1] or force_load:
             self.cursor.execute(TRUNCATE_TABLE_QUERY)
             self._load_csv_data_to_table()
@@ -73,6 +81,7 @@ class EcommerceSale:
             pass
 
     def _load_csv_data_to_table(self):
+        """Handles the logic for loadig data from CSV to database table, inserts 10000 records on every write op"""
         insert_query_keys = list(map(lambda x: x[1][0], CSV_TO_DB_KEY_TYPE_MAPPING.items()))
         for batch_of_rows in self._fetch_csv_data_as_batches_of_10000():
             formatted_insert_query = DATA_LOADING_QUERY % (
@@ -87,18 +96,21 @@ class EcommerceSale:
             self.cnx.commit()
 
     def fetch_most_purchased_customers(self):
+        """Fetches most purchased customers"""
         self.cursor.execute(CUSTOMER_PURCHASED_HIGHEST_QUANTITY_QUERY)
         result = list(self.cursor)
 
         return result
 
     def fetch_top10_customers_who_spent_most(self):
+        """Fetches top10 most spent customers"""
         self.cursor.execute(TOP_10_CUSTOMERS_WITH_SALES_AMOUNT)
         result = list(self.cursor)
 
         return result
 
     def fetch_most_purchased_products(self):
+        """Fetches top10 most purchased products"""
         self.cursor.execute(TOP_10_PRODUCTS_PURCHASES)
         result = list(self.cursor)
 
@@ -106,6 +118,7 @@ class EcommerceSale:
 
     @staticmethod
     def _fetch_csv_data_as_batches_of_10000():
+        """Acts like an iterator, and provides 10000 records on every iteration"""
         file = open("./data/data.csv", encoding="ISO-8859-1")
         reader = csv.DictReader(file, delimiter=",")
 
@@ -122,6 +135,7 @@ class EcommerceSale:
 
     @staticmethod
     def create_db_connection():
+        """Creates a database connection"""
         return mysql.connector.connect(
             host=os.environ["DB_HOST_NAME"],
             user=os.environ["DB_USER"],
@@ -131,6 +145,7 @@ class EcommerceSale:
 
 
 def main(force_load):
+    """main function only handles the high level logic of the program"""
     ecommerce_sale = EcommerceSale()
     ecommerce_sale.load_data(force_load)
 
@@ -157,4 +172,5 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
+    # If force load is true, then the table gets truncated and reloaded
     main(force_load=bool(re.match("^true|0$", args.force_load.lower())))
